360008a03be0c0ca1bf5449c8cf767fcc322745e
SelkaChan <eduardochineacl@gmail.com>
2023-01-23 19:01:17 +0000

    Proyecto iniciado sin vistas

--- public/index.php
+++ public/index.php
@@ -1,1 +1,51 @@
-<h1>Hola</h1>
+<?php
+require_once('../vendor/autoload.php');
+
+//Vistas
+use Philo\Blade\Blade;
+$views='../src/views';
+$cache='../cache';
+
+$blade = new Blade($views,$cache);
+
+//Router system
+$router = new AltoRouter();
+
+// List of routes
+// $router->map('GET', '/',  function(){
+//     echo "Hola enrutamiento";
+// });
+
+// $router->map('GET','/',function(){
+//     global $blade;
+//     echo $blade->view()->make('home')->render();
+// });
+
+$router->map('GET','/','home');
+$router->map('GET', '/user', 'userController#index');
+$router->map('GET', '/user/[i:id]', 'userController#show');
+
+// End of list
+
+$match = $router->match();
+if($match) {
+ $target = $match["target"];
+ if(is_string($target) && strpos($target, "#") !== false) {
+     list($controller, $action) = explode("#", $target);
+     $controller = "Dsw\Ifriend\controllers\\" . $controller;
+     $controller = new $controller;
+     $controller->$action($match["params"]);
+ } else {
+     if(is_callable($match["target"])) 
+call_user_func_array($match["target"], $match["params"]);
+     else{
+        echo $blade->view()->make($match["target"])->render();
+     }
+ }
+} else {
+ echo "Ruta no válida";
+ die();
+}
+
+
+?>
